1b3539125c2780267fa938342328995ec38994ef
EnzoLopo <enzo.lopo@hotmail.com>
2022-06-09 14:28:48 -0300

    push

--- src/Pacote1/Aluno.java
+++ src/Pacote1/Aluno.java
@@ -5,8 +5,8 @@ public class Aluno extends Pessoa {
     private double Mensalidade;
     private int Materias;
 
-    public Aluno (long ra, int Materias, String Nome, String Enderço, String DatNasci, long Telefone, int CPF, double valAula){
-        super(Nome, Enderço, DatNasci, Telefone, CPF, valAula);
+    public Aluno (long ra, int Materias, String Nome, String Endereço, String DatNasci, long Telefone, int CPF, double valAula){
+        super(Nome, Endereço, DatNasci, Telefone, CPF);
         this.ra = ra;
         this.Materias = Materias;
     }
